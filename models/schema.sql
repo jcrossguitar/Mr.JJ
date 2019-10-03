01d3994aef28c936c2df81e539f6d9e3e0cb54e3
jon <jcrossguitar@gmail.com>
2019-10-02 19:29:48 -0700

    I created sql files for a db and a table with info

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,17 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS flashcards_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE flashcards_db;
+
+USE flashcards_db;
+
+-- Create the database task_saver_db and specified it for use.
+
+
+-- Create the table tasks.
+CREATE TABLE cards (
+  id INT NOT NULL AUTO_INCREMENT,
+  cardTitle varchar(255) NOT NULL,
+  subjectName varchar(500)  NOT NULL,
+  content varchar(1000) NOT NULL,
+  PRIMARY KEY (id)
+);
---
+++ models/seeds.sql
@@ -0,0 +1,8 @@
+
+USE flashcards_db;
+
+-- Insert a set of records.
+INSERT INTO cards (cardTitle, cardName, content) VALUES ('Anatomy', "Cranium", "The head yo");
+INSERT INTO cards (cardTitle, cardName, content) VALUES ('Algebra', "2 + 2 = ", "4");
+INSERT INTO cards (cardTitle, cardName, content) VALUES ('Music', "Treble Clef", "Starts the piece of music in the standard format EGBDF");
+INSERT INTO cards (cardTitle, cardName, content) VALUES ('Code', "variable", "a variable can be anything. ex var x = 5 + 9");
