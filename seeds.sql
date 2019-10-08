b6f1bfc4f8cdf5000262c946f94cb2300f2b02be
Max Reinmueller <maxreinmueller@gmail.com>
2019-10-07 17:30:09 -0700

    database stuff

---
+++ seeds.sql
@@ -0,0 +1,7 @@
+
+USE flashee;
+
+INSERT INTO cards (question, answer) VALUES ('What does HTML stand for?', "Hypertext Markup Language");
+INSERT INTO cards (question, answer) VALUES ('What does JSON stand for?', "Javascript Object Notation");
+INSERT INTO cards (question, answer) VALUES ('What does CSS stand for?', "Cascading Style Sheet");
+INSERT INTO cards (question, answer) VALUES ('What does API stand for?', "Applicaiton Programming Interface");
